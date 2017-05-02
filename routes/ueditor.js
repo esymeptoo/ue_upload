bdb9bd31582da1f21ecea809889886a32e5fd1ba
周驰 <zhouchi@shouqianba.com>
2017-05-02 12:50:09 +0800

    上传延迟

--- routes/ueditor.js
+++ routes/ueditor.js
@@ -4,6 +4,8 @@ const Busboy = require('busboy');
 const fs = require('fs');
 const path = require('path'),
     os = require('os');
+const formidable = require('formidable');
+
 
 router.get('/ueditor', (req, res) => {
     // 客户端发起其它请求
@@ -14,23 +16,48 @@ router.get('/ueditor', (req, res) => {
 router.post('/ueditor', (req, res) => {
     if (req.query.action === 'uploadimage') {
         console.log('上传图片');
+        //var form = new formidable.IncomingForm();
+        //form.uploadDir = __dirname.split('/routes')[0] + '/public/images/upload/';
+        //form.keepExtensions = true; //保留后缀
+        //form.maxFieldsSize = 2 * 1024; //文件大小
+        //form.parse(req, function (error, fields, files) {
+        //    if (error) {
+        //        console.log(error.message);
+        //        return;
+        //    }
+        //    var type_of_image = files.upfile.type;
+        //    const last_name = type_of_image.split('/')[1];
+        //    //新文件名
+        //    var saveName = 'upload_' + (new Date()).getTime() + '.' + last_name;
+        //    //保存地址
+        //    var savePath = form.uploadDir + saveName;
+        //    fs.renameSync(files.upfile.path, savePath);
+        //    res.json({
+        //        'url': `/images/upload/${saveName}`,
+        //        'title': req.body.pictitle,
+        //        'original': saveName,
+        //        'state': 'SUCCESS'
+        //    });
+        //})
         var name = '',
             file_name = '';
         var busboy = new Busboy({ headers: req.headers });
         busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
             name = `upload_${(new Date()).getTime()}.${mimetype.split('/')[1]}`;
             file_name = filename;
-            var saveTo = path.join(`${os.tmpDir()}/public`, path.basename(`upload_${(new Date()).getTime()}.${mimetype.split('/')[1]}`));
+            var saveTo = path.join(`${os.tmpDir()}/public/images/upload/`, path.basename(`upload_${(new Date()).getTime()}.${mimetype.split('/')[1]}`));
             file.pipe(fs.createWriteStream(saveTo));
         });
         busboy.on('finish', function() {
             console.log('over');
-            res.json({
-                'url': `/${name}`,
-                'title': req.body.pictitle,
-                'original': file_name,
-                'state': 'SUCCESS'
-            });
+            setTimeout(function() {
+                res.json({
+                    'url': `/images/upload/${name}`,
+                    'title': req.body.pictitle,
+                    'original': file_name,
+                    'state': 'SUCCESS'
+                });
+            }, 3000);
         });
         req.pipe(busboy);
     }
